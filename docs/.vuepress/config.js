c2d83078a00dd138d354a3bb85b1b48f4beafa7b
zhanglx <zhanglx@fabledt.com>
2022-09-30 15:50:38 +0800

    修改配置打包部署linux

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,19 @@
-const head = require('./config/head.js');
-const plugins = require('./config/plugins.js');
-const themeConfig = require('./config/themeConfig.js');
+const head = require("./config/head.js");
+const plugins = require("./config/plugins.js");
+const themeConfig = require("./config/themeConfig.js");
 
 module.exports = {
-  theme: 'vdoing', // 使用依赖包主题
+  theme: "vdoing", // 使用依赖包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
-
+  base: "/lakeiedward/",//部署之后访问得路径
   title: "lakeiedward's",
-  description: 'web前端技术博客，专注web',
+  description: "web前端技术博客，专注web",
   // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
-    extractHeaders: [ 'h2', 'h3', 'h4' ]
+    extractHeaders: ["h2", "h3", "h4"],
   },
   head,
   plugins,
   themeConfig,
-}
+};
