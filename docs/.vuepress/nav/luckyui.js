ab2e91274ae24244100820d0226ad618490ba58c
lakei <1136725653@qq.com>
2023-01-12 12:02:37 +0800

    更新配置

--- docs/.vuepress/nav/luckyui.js
+++ docs/.vuepress/nav/luckyui.js
@@ -1,7 +1,6 @@
 // 前端航栏
 module.exports = {
   text: "作品",
-  link: "/luckyui/", // 目录页链接，此处 link 是 vdoing 主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
   items: [
     // 没有二级导航时可以直接添加
     // { text: "vue-luckyui", link: "/luckyui/home/" },
